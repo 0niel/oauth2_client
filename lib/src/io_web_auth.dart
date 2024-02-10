0f7199f95c2bf255deec90d85955fa3002f4dd3a
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-02-10 14:38:14 +0300

    Update io_web_auth.dart

--- lib/src/io_web_auth.dart
+++ lib/src/io_web_auth.dart
@@ -4,6 +4,40 @@ import 'base_web_auth.dart';
 
 BaseWebAuth createWebAuth() => IoWebAuth();
 
+const _defaultLandingPage = '''
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Access Granted</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html, body { margin: 0; padding: 0; }
+
+    main {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-height: 100vh;
+      font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+    }
+
+    #text {
+      padding: 2em;
+      text-align: center;
+      font-size: 2rem;
+    }
+  </style>
+</head>
+<body>
+  <main>
+    <div id="text">Вы можете закрыть это окно</div>
+  </main>
+</body>
+</html>
+''';
+
 class IoWebAuth implements BaseWebAuth {
   @override
   Future<String> authenticate(
@@ -16,6 +50,7 @@ class IoWebAuth implements BaseWebAuth {
       url: url,
       options: FlutterWebAuth2Options(
         preferEphemeral: (opts?['preferEphemeral'] == true),
+        landingPageHtml: _defaultLandingPage,
       ),
     );
   }
